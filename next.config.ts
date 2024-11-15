1b2596b57af7fdd2e9f3c965ffd92cbf5e0737a4
Ari <ariapero@mit.edu>
2024-11-15 00:39:36 -0500

    Fixes for GithubPages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Ensure that the base path matches your GitHub Pages URL structure
+  basePath: '/ariapero.github.io',
+  assetPrefix: '/ariapero.github.io/',
 };
 
 export default nextConfig;
